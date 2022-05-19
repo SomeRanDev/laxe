c6663f753aa254953c0302f3f513421ee81cd685
RoBBoR <28355157+RobertBorghese@users.noreply.github.com>
2022-05-19 15:10:59 -0400

    added more laxetypedef helpers

--- src/laxe/ast/LaxeTypeDefinition.hx
+++ src/laxe/ast/LaxeTypeDefinition.hx
@@ -33,27 +33,110 @@ abstract LaxeTypeDefinition(TypeDefinition) from TypeDefinition to TypeDefinitio
 		});
 	}
 
-    public inline function addFunction(expr: LaxeExpr, name: Null<String> = null) {
-        switch(expr.expr) {
-            case EFunction(kind, f): {
-                final name = switch(kind) {
-                    case FNamed(name, _): name;
-                    case _: name;
-                }
-                #if macro
-                if(name == null) {
-                    Context.error("Unable to determine function name", expr.pos);
-                    return;
-                }
-                #end
-                this.fields.push({
-                    name: name != null ? name : "unnamed",
-                    pos: expr.pos,
-                    kind: FFun(f),
-                    access: [APublic]
-                });
-            }
-            case _:
-        }
-    }
+	public inline function addFunction(expr: LaxeExpr, name: Null<String> = null) {
+		switch(expr.expr) {
+			case EFunction(kind, f): {
+				final name = switch(kind) {
+					case FNamed(name, _): name;
+					case _: name;
+				}
+				#if macro
+				if(name == null) {
+					Context.error("Unable to determine function name", expr.pos);
+					return;
+				}
+				#end
+				this.fields.push({
+					name: name != null ? name : "unnamed",
+					pos: expr.pos,
+					kind: FFun(f),
+					access: [APublic]
+				});
+			}
+			case _:
+		}
+	}
+
+	public inline function isEnum(): Bool {
+		return switch(this.kind) {
+			case TDEnum: true;
+			case _: false;
+		}
+	}
+
+	public inline function isStructure(): Bool {
+		return switch(this.kind) {
+			case TDStructure: true;
+			case _: false;
+		}
+	}
+
+	public inline function isClass(): Bool {
+		return switch(this.kind) {
+			case TDClass(_, _, _, _, _): true;
+			case _: false;
+		}
+	}
+
+	public inline function getClass() {
+		return switch(this.kind) {
+			case TDClass(superClass, interfaces, isInterface, isFinal, isAbstract): {
+				superClass: superClass,
+				interfaces: interfaces,
+				isInterface: isInterface,
+				isFinal: isFinal,
+				isAbstract: isAbstract
+			}
+			case _: throw "Not a TDClass";
+		}
+	}
+
+	public inline function isTypedef(): Bool {
+		return switch(this.kind) {
+			case TDAlias(_): true;
+			case _: false;
+		}
+	}
+
+	public inline function getTypedef() {
+		return switch(this.kind) {
+			case TDAlias(t): t;
+			case _: throw "Not a TDAlias";
+		}
+	}
+
+	public inline function isWrapper(): Bool {
+		return switch(this.kind) {
+			case TDAbstract(_, _, _): true;
+			case _: false;
+		}
+	}
+
+	public inline function getWrapper() {
+		return switch(this.kind) {
+			case TDAbstract(tthis, from, to): {
+				tthis: tthis,
+				from: from,
+				to: to
+			};
+			case _: throw "Not a TDAbstract";
+		}
+	}
+
+	public inline function isField(): Bool {
+		return switch(this.kind) {
+			case TDField(_, _): true;
+			case _: false;
+		}
+	}
+
+	public inline function getField() {
+		return switch(this.kind) {
+			case TDField(kind, access): {
+				kind: kind,
+				access: access
+			};
+			case _: throw "Not a TDField";
+		}
+	}
 }
