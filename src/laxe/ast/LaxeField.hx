bebdc51f89cd89a5920b3057f5dbd7e0440a3534
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2022-08-17 19:53:18 -0400

    add and test onField decor

--- src/laxe/ast/LaxeField.hx
+++ src/laxe/ast/LaxeField.hx
@@ -1,20 +1,67 @@
 package laxe.ast;
 
+import haxe.macro.Expr.ComplexType;
 import haxe.macro.Expr.Field;
 
 @:nullSafety(Strict)
 @:forward
 @:remove
 abstract LaxeField(Field) from Field to Field {
-    public inline function new(f: Field) {
-        this = f;
-    }
+	public inline function new(f: Field) {
+		this = f;
+	}
 
-    public function getExpr(): Null<LaxeExpr> {
-        return switch(this.kind) {
-            case FVar(_, e): e;
-            case FFun({ expr: e }): e;
-            case FProp(_, _, _, e): e;
+	public function getExpr(): Null<LaxeExpr> {
+		return switch(this.kind) {
+			case FVar(_, e): e;
+			case FFun({ expr: e }): e;
+			case FProp(_, _, _, e): e;
+		}
+	}
+
+    public inline function setExpr(e: LaxeExpr) {
+        this.kind = switch(this.kind) {
+            case FVar(t, _): FVar(t, e);
+			case FFun(fun): {
+                fun.expr = e;
+                FFun(fun);
+            }
+			case FProp(get, set, t, _): FProp(get, set, t, e);
         }
     }
+
+	public function isVar(): Bool {
+		return switch(this.kind) {
+			case FVar(_, _): true;
+			case _: false;
+		}
+	}
+
+    public function getVarType(): Null<ComplexType> {
+        return switch(this.kind) {
+			case FVar(t, _): t;
+			case _: null;
+		}
+    }
+
+	public function isFunction(): Bool {
+		return switch(this.kind) {
+			case FFun(_): true;
+			case _: false;
+		}
+	}
+
+    public function getFunctionReturnType(): Null<ComplexType> {
+        return switch(this.kind) {
+			case FFun({ ret: t }): t;
+			case _: null;
+		}
+    }
+
+    public function isProperty(): Bool {
+		return switch(this.kind) {
+			case FProp(_, _, _, _): true;
+			case _: false;
+		}
+	}
 }
